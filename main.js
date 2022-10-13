34ad880f85b89ca365d2880adce44a11762e941c
Freddy Haas <email@frederik-haas.de>
2022-10-13 17:00:01 +0200

    Receive user's choice

--- main.js
+++ main.js
@@ -20,10 +20,22 @@ function getComputerChoice () {
     return computerChoice;
 }
 console.log (getComputerChoice());
+
 /* 2. Get from user a random choice of "Rock", "Paper", "Scissors" */
+function getPlayerSelection () {
     /* 2.1 Prompt user for input */
-    /* 2.2 Clean up user input and store in variable */
+    let userInput = prompt ("Now you choose: rock, paper or scissor?");
+    /* 2.2 Clean up user input and show prompt again if input is not valid*/
+    let cleanUserInput = userInput.toLowerCase();
+    while (!(cleanUserInput === "rock" || cleanUserInput === "paper" || cleanUserInput === "scissor")) {
+        userInput = prompt ("Invalid input! Type: rock, paper or scissor");
+        cleanUserInput = userInput.toLowerCase();
+    }
     /* 2.3 Return result */
+    return cleanUserInput;
+}
+console.log (getPlayerSelection());
+
 /* 3. Compare user's vs. computer's choice, update counts for user and computer, output winner */
     /* 3.1 If user's choice = computer's choice return draw and do not update count */
     /* 3.2 User (u) and computer (c): If u = "Paper" & c = "Rock" OR u = "Scissor" & c = "Paper" OR u ="Rock" & "Scissor" -> return player wins and update count*/
