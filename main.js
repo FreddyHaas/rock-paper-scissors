47ff99201a7054f2cd9f9dbea0a2072a4b2933e9
Freddy Haas <email@frederik-haas.de>
2022-10-13 15:43:05 +0200

    Write pseudocode

---
+++ main.js
@@ -0,0 +1,13 @@
+/* 1. Get from computer a random choice of "Rock", "Paper", "Scissors" */
+    /* 1.1 Generate random number between one and three */
+    /* 1.2 Associate numer with 0 = "Rock"; 1 ="Paper" ; 2 = "Scissors" */
+    /* 1.3 Return result */
+/* 2. Get from user a random choice of "Rock", "Paper", "Scissors" */
+    /* 2.1 Prompt user for input */
+    /* 2.2 Clean up user input and store in variable */
+    /* 2.3 Return result */
+/* 3. Compare user's vs. computer's choice, update counts for user and computer, output winner */
+    /* 3.1 If user's choice = computer's choice return draw and do not update count */
+    /* 3.2 User (u) and computer (c): If u = "Paper" & c = "Rock" OR u = "Scissor" & c = "Paper" OR u ="Rock" & "Scissor" -> return player wins and update count*/
+    /* 3.3 Else return computer wins and update count */
+/* 4. Repeat steps one to three until user or computer reaches five wins and declare overall winner */
