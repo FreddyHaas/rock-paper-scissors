cd8a289490eba6d589806f5a1411427a38c9a5c8
Freddy Haas <email@frederik-haas.de>
2022-10-17 21:49:02 +0200

    Update UI to buttons

--- main.js
+++ main.js
@@ -3,21 +3,32 @@
 
 let scorePlayer = 0;
 let scoreComputer = 0;
+const outputComputer = document.querySelector("#outputComputer");
+const outputPlayer = document.querySelector("#outputPlayer");
+const interimResult = document.querySelector("#interimResult");
+const finalResult = document.querySelector('#finalResult');
 
-while (scorePlayer < 5 && scoreComputer < 5 ) {
+const btn1 = document.querySelector("#btn1");
+btn1.addEventListener('click', () => {
+    let playerSelection = "rock";
     let computerSelection = getComputerChoice();
-    let playerSelection = getPlayerSelection();
-    console.log(playRound(playerSelection, computerSelection));
-    console.log (scorePlayer);
-    console.log (scoreComputer);
-} 
+    playRound(playerSelection, computerSelection);
+});
 
-if (scorePlayer === 5) {
-    console.log("Congrats, you win!");
-}
-else {
-    console.log("You lost, try again :) ");
-}
+const btn2 = document.querySelector("#btn2");
+btn2.addEventListener('click', () => {
+    let playerSelection = "paper";
+    let computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+});
+
+const btn3 = document.querySelector("#btn3");
+btn3.addEventListener('click', () => {
+    let playerSelection = "scissors";
+    let computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+});
+
 /* FUNCTIONS */
 
 /* 1. Get from computer a random choice of "Rock", "Paper", "Scissors" */
@@ -40,35 +51,40 @@ function getComputerChoice () {
     /* 1.3 Return result */
     return computerChoice;
 }
-
-/* 2. Get from user a random choice of "Rock", "Paper", "Scissors" */
-function getPlayerSelection () {
-    /* 2.1 Prompt user for input */
-    let userInput = prompt ("Now you choose: rock, paper or scissors?");
-    /* 2.2 Clean up user input and show prompt again if input is not valid*/
-    let cleanUserInput = userInput.toLowerCase();
-    while (!(cleanUserInput === "rock" || cleanUserInput === "paper" || cleanUserInput === "scissors")) {
-        userInput = prompt ("Invalid input! Type: rock, paper or scissors");
-        cleanUserInput = userInput.toLowerCase();
-    }
-    /* 2.3 Return result */
-    return cleanUserInput;
-}
-
-/* 3. Compare user's vs. computer's choice, update counts for user and computer, output winner */
+   
+/* 2. Compare user's vs. computer's choice, update counts for user and computer, output winner */
 function playRound (playerSelection, computerSelection) {  
+    /* 3.0 Set result from previous round to zero */
+    finalResult.textContent = "";
+
     /* 3.1 If user's choice = computer's choice return draw and do not update count */
     if (playerSelection === computerSelection) {
-        return `Draw! You both chose ${playerSelection}.`;
+        interimResult.textContent = `Draw! You both chose ${playerSelection}.`;
     }
     /* 3.2 User (u) and computer (c): If u = "Paper" & c = "Rock" OR u = "Scissors" & c = "Paper" OR u ="Rock" & "Scissors" -> return player wins and update count*/
     else if (playerSelection === "paper" & computerSelection === "rock" || playerSelection === "scissors" & computerSelection === "paper" || playerSelection === "rock" & computerSelection === "scissors") {
         scorePlayer++;
-        return `You win! ${playerSelection} beats ${computerSelection}.`;
+        interimResult.textContent = `You win! ${playerSelection} beats ${computerSelection}.`;
     }
     /* 3.3 Else return computer wins and update count */
     else {
         scoreComputer++;
-        return `You lose! ${computerSelection} beats ${playerSelection}.`;
+        interimResult.textContent = `You lose! ${computerSelection} beats ${playerSelection}.`;
+    }
+    /* 3.4 Show scores and output winner */
+    outputComputer.textContent = `Computers' score: ${scoreComputer}`;
+    outputPlayer.textContent = `Players' score: ${scorePlayer}`;
+
+    /* 3.5 Show overall winner */
+    if (scorePlayer === 5) {
+        finalResult.textContent = "Game over! You win!";
+        scorePlayer = 0;
+        scoreComputer = 0;
+    }
+
+    if (scoreComputer === 5) {
+        finalResult.textContent = "Oh no, you lost! Try again :)";
+        scorePlayer = 0;
+        scoreComputer = 0;
     }
 }
